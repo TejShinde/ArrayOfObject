43abf8cb31a6a11af8357c3e3cc86f62374bdd79
TejShinde <shindetejal995@gmail.com>
2024-11-19 23:40:37 +0530

    Add project files.

---
+++ ArrayOfObject/EmpMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfObject
+{
+    internal class EmpMain
+    {
+        //static void Main(string[] args)
+        //{
+        //    // array of emp's
+        //   // int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        //    Employee[] employees = new Employee[5]
+        //    {
+        //        new Employee{Id=1,Name="Amol",Salary=45000},//0
+        //        new Employee{Id=2,Name="Akshay",Salary=15000},
+        //        new Employee{Id=3,Name="Suraj",Salary=55000},
+        //        new Employee{Id=4,Name="Rohan",Salary=25700},
+        //        new Employee{Id=5,Name="Vishal",Salary=35000},
+        //    };
+        //    // Console.WriteLine(employees[3]);
+
+        //    //for (int i = 0; i < employees.Length; i++)
+        //    //{
+        //    //    Console.WriteLine(employees[i]);
+        //    //}
+        //    Console.WriteLine("----------using foreach-------------");
+        //    foreach (Employee emp in employees)
+        //    {
+        //        Console.WriteLine(emp);
+        //    }
+        //}
+    }
+}
---
+++ ArrayOfObject/ProductMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfObject
+{
+    internal class ProductMain
+    {
+        static void Main(string[] args)
+        {
+            Product[] prod = new Product[5]
+            {
+            new Product { Id = 1, Name = "Laptop", Price = 1200 },
+            new Product { Id = 2, Name = "Mouse", Price = 50 },
+            new Product { Id = 3, Name = "Keyboard", Price = 150 },
+            new Product { Id = 4, Name = "Monitor", Price = 700 },
+            new Product { Id = 5, Name = "Printer", Price = 300 }
+            };
+
+            Console.WriteLine("Displaying all products using for loop:");
+            for (int i = 0; i < prod.Length; i++)
+            {
+                Console.WriteLine(prod[i]);
+            }
+
+
+            Console.WriteLine("\nDisplaying all products using foreach loop:");
+            foreach (Product p in prod)
+            {
+                Console.WriteLine(p);
+            }
+
+            
+            Console.WriteLine("\nDisplaying products with price > 500:");
+            foreach (Product p in prod)
+            {
+                if (p.Price > 500)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+    }
+}
